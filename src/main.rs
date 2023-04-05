e23086405ab24adb18a60e3036628b6e455bede7
Nurudeen Abdul-Karim <nurudeen.karim2016@gmail.com>
2023-04-05 00:19:39 +0000

    rust guessing game with colors

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+use colored::*;
+
+fn main() {
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    
+    loop{
+        println!("Input your guess:");
+    
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess:u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid input");
+                continue;
+            }
+        };
+        
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("{}","Too small".red()),
+            Ordering::Greater => println!("{}","Too big".red()),
+            Ordering::Equal => {
+                println!("{}","You win".green());
+                break;
+            }
+        }
+        
+        println!("Try again!")
+
+    }
+
+
+}
+
